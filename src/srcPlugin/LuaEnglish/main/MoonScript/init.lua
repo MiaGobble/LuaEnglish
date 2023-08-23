2453b7decb9dd6dc0243976ad52624545db6f1af
Iconic Gaming <igottic.studios@gmail.com>
2023-08-22 19:54:34 -0500

    Bye moonscript you will not be missed

--- src/srcPlugin/LuaEnglish/main/MoonScript/init.lua
+++
@@ -1,5 +0,0 @@
-do
-  local _with_0 = require(script.Parent.base)
-  _with_0.insert_loader()
-  return _with_0
-end
--- src/srcPlugin/LuaEnglish/main/MoonScript/transform.lua
+++
@@ -1,4 +0,0 @@
-return {
-  Statement = require(script.Parent.transform.statement),
-  Value = require(script.Parent.transform.value)
-}
